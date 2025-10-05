7888328ffc1cf31b51a1cb163fbe8d087ea5eef6
mostafakt <mostafa.ktan.5@gmail.com>
2025-10-05 19:26:47 +0300

    fix

--- src/components/home/hero-section/Hero.tsx
+++ src/components/home/hero-section/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "@/components/ui/Image";
 import EmblaCarousel from "../../ui/Carousel";
+import { getTranslations } from "next-intl/server";
 
 const HERO_IMAGES = [
   `/images/homepage/hero/18.webp`,
@@ -14,10 +15,8 @@ const HERO_IMAGES = [
 ];
 
 const HERO_CONTENT = {
-  title:
-    "اكتشف عالم الذهب مع لايف جولد التطبيق الأول في السعودية لمتابعة أسعار الذهب على مدار الساعة",
-  description:
-    "نقدم لك تحديثات دقيقة وفورية لأسعار الذهب في السوق المحلّي والعالمي، مما يساعد أصحاب المتاجر والمستثمرين على اتخاذ قرارات دقيقة وصائبة في وقت قياسي. اكتشف عالم الذهب مع لايف جولد التطبيق الأول في السعودية لمتابعة أسعار الذهب على مدار الساعة، نقدم لك تحديثات دقيقة وفورية لأسعار الذهب في السوق المحلّي والعالمي، مما يساعد أصحاب المتاجر والمستثمرين على اتخاذ قرارات دقيقة وصائبة في وقت قياسي.",
+  title: "hero.title",
+  description: "hero.description",
 };
 
 const HeroSlide = ({ src }: { src: string }) => (
@@ -33,30 +32,39 @@ const HeroSlide = ({ src }: { src: string }) => (
   </div>
 );
 
-const HeroOverlay = () => (
-  <div className="absolute inset-0 z-50 flex items-center justify-center bg-secondary-dark bg-opacity-40">
-    <div className="text-center max-w-4xl mx-auto px-4 lg:px-8 xl:px-12 2xl:max-w-6xl flex flex-col items-start justify-start ">
-      <h1
-        style={{ animationDelay: "720ms" }}
-        className="text-primary-500 text-lg w-full lg:w-3/4 text-center md:text-start lg:text-xl xl:text-2xl 2xl:text-3xl font-medium lg:font-semibold xl:font-bold mb-4  animate-fade-up lg:mb-6 !leading-8 lg:!leading-[60px] tracking-widest"
-      >
-        {HERO_CONTENT.title}
-      </h1>
-      <p
-        style={{ animationDelay: "900ms" }}
-        className="text-white text-center md:text-start mt-6 lg:mt-12 text-sm lg:text-base xl:text-lg 2xl:text-xl font-normal !leading-5 lg:!leading-10 opacity-95 animate-fadeUp"
-      >
-        {HERO_CONTENT.description}
-      </p>
+const HeroOverlay = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div className="absolute inset-0 z-50 flex items-center justify-center bg-secondary-dark bg-opacity-40">
+      <div className="text-center max-w-4xl mx-auto px-4 lg:px-8 xl:px-12 2xl:max-w-6xl flex flex-col items-start justify-start ">
+        <h1
+          style={{ animationDelay: "720ms" }}
+          className="text-primary-500 text-lg w-full lg:w-3/4 text-center md:text-start lg:text-xl xl:text-2xl 2xl:text-3xl font-medium lg:font-semibold xl:font-bold mb-4  animate-fade-up lg:mb-6 !leading-8 lg:!leading-[60px] tracking-widest"
+        >
+          {title}
+        </h1>
+        <p
+          style={{ animationDelay: "900ms" }}
+          className="text-white text-center md:text-start mt-6 lg:mt-12 text-sm lg:text-base xl:text-lg 2xl:text-xl font-normal !leading-5 lg:!leading-10 opacity-95 animate-fadeUp"
+        >
+          {description}
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const generateSlides = () =>
   HERO_IMAGES.map((imageSrc) => <HeroSlide key={imageSrc} src={imageSrc} />);
 
-export default function HeroSection() {
+export default async function HeroSection() {
   const slides = generateSlides();
+  const t = await getTranslations();
 
   return (
     <section
@@ -64,7 +72,10 @@ export default function HeroSection() {
       role="banner"
       aria-label="Hero section"
     >
-      <HeroOverlay />
+      <HeroOverlay
+        title={t(HERO_CONTENT.title)}
+        description={t(HERO_CONTENT.description)}
+      />
       <EmblaCarousel fullWidth hideDots slides={[...slides]} />
     </section>
   );
